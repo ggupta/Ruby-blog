2fbad0192444c4b9df79c7f76d5c7024c5555cda
Gaurav Gupta <ggupta@splunk.com>
2010-11-08 11:08:00 -0800

    test commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '9e9912b50ddccb1ba854090562cccee45b10747d507fc3962aa1a30d78886cc1e7af15e28b71f6b0dfcb2ad9c65588bed9e0509f4c6556023d193cd66be4116d'
